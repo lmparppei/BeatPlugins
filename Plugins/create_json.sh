7111a5209f086a8f9163ef38a4747b63d82c2ad6
Lauri-Matti Parppei <lauri-matti@kapitan.fi>
2021-04-19 16:32:54 +0300

    Packing command

---
+++ Plugins/create_json.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+IFS=$'\n'
+#files=( $(find . -maxdepth 1 -mindepth 1  -type d) )
+
+json="{"
+
+i=0
+files=(*/)
+count=${#files[@]}
+echo "Distributing $count plugins..."
+
+for dir in */
+	do 
+	filename=${dir//\//}
+
+	echo "   $filename"
+
+	version=$(find "./$dir$filename" -type f -exec grep "Version:" {} \;);
+	version=${version/Version: /}
+	description=$(find "./$dir$filename" -type f -exec grep "Description:" {} \;);
+	description=${description/Description: /}
+	copyright=$(find "./$dir$filename" -type f -exec grep "Copyright:" {} \;);
+	copyright=${copyright/Copyright: /}
+	
+	json+="		\"$filename\": { \"version\": \"$version\", \"copyright\": \"$copyright\", \"description\": \"$description\" }"
+
+	i=$(($i + 1))
+	if [[ $i -lt $count ]]
+		then
+			json+=","
+	fi
+
+	zip -vrq "../Dist/$filename.zip" "./$dir"
+done
+json+="}"
+
+echo $json > "../Dist/Beat Plugins.json"
+echo "Written JSON file"
