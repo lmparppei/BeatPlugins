7226472b1eadecbd20271714c1514dc1ea2e921e
lmparppei <lauri-matti@kapitan.fi>
2024-05-21 22:14:09 +0300

    Import Markdown

---
+++ Plugins/Import Markdown.beatPlugin/plugin.js
@@ -0,0 +1,37 @@
+/*
+
+Import Markdown
+Version: 1.0
+Description: Import Markdown files and convert them to Fountain
+Copyright: Lauri-Matti Parppei 2021
+Type: Import
+<Description>
+	<p>Import Markdown files and convert them to Fountain. Replaces basic formatting, converts comments to notes, and multi-line comment blocks to omissions.</p>
+	<p>When the mport plugin is installed, you'll see Markdown under <em>File → Import</em>.</p>
+</Description>
+
+*/
+
+Beat.openConsole()
+
+Beat.importHandler(["md"], function (path) {
+	if (path) {
+		importMd(Beat.fileToString(path))
+	}
+})
+
+function importMd(contents) {
+	const noteRegex = /%%(.*)%%/g
+	const omitRegex = /%%((.|\n)*)%%/gm
+	const boldRegex = /__((.)*)__/gm
+	const italicRegex = /_((.)*)_/gm
+
+	// Replace notes
+	let text = contents
+	text = text.replace(noteRegex, "[[$1]]")
+	text = text.replace(omitRegex, "/*$1*/")
+	text = text.replace(boldRegex, "/**$1**/")
+	text = text.replace(italicRegex, "/*$1*/")
+
+	Beat.newDocument(text)
+}
